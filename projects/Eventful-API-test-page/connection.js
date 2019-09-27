08845c2d3848c38394cab68fc8580875ebc2dfa0
julinvictus <50844569+julinvictus@users.noreply.github.com>
2019-09-27 09:01:41 -0700

    Updated to postgres (#952)

--- projects/Eventful-API-test-page/connection.js
+++ projects/Eventful-API-test-page/connection.js
@@ -1,13 +1,16 @@
 //require packages
 const pg = require("pg");
-const pgKey = require('./keys').dbPassword;
+const pgKey = require('./keys').pg;
+const Pool = require('pg').Pool;
 
-const pool = new pg.Pool({
+ //create PostgreSQL connection
+ const connection = new Pool({
+  
   host: "localhost",
   port: 5432,
-  user: "eventonica",
-  password: pgKey,
-  database: "eventonica"
+  user: "XXXXXXX", // type your username here
+  password: "XXXXXXX", // type your username here
+  database: "XXXXXXX" // type your database name here
 });
 
-module.exports = pool;
+module.exports = connection;
