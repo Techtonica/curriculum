557513f350a30d72fbdf6b02766386ffd5441f8d
Bill DePhillips <355438+vegetabill@users.noreply.github.com>
2020-02-04 13:22:57 -0800

    Create jasmine-pair-activity.js

---
+++ testing-and-tdd/jasmine-pair-activity.js
@@ -0,0 +1,18 @@
+/*
+Take this source file as a starting point.
+
+Implement a function called invert
+
+var object = { 'a': 1, 'b': 2, 'c': 1 };
+ 
+invert(object);
+// => { '1': 'c', '2': 'b' }
+
+Implement this function and then create Jasmine tests to verify its 
+correctness. Think of all the input that could come in and decide
+what to do with them via tests.
+*/
+
+function invert() {
+
+}
