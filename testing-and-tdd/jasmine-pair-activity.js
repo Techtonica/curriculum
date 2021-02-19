f4ac6e53e269b21e42a1e67ec0cbe59019847cae
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-02-18 17:07:32 -0800

    Update jasmine-pair-activity.js

--- testing-and-tdd/jasmine-pair-activity.js
+++ testing-and-tdd/jasmine-pair-activity.js
@@ -1,18 +1,25 @@
 /*
 Take this source file as a starting point and copy it into a new folder in your personal assignments repo.
-
 Implement a function called invert.
-
 Creates an object composed of the inverted keys and values of object. If object contains duplicate values, subsequent values overwrite property assignments of previous values.
-
 let object = { 'a': 1, 'b': 2, 'c': 1 };
  
 invert(object);
-// => { '1': 'c', '2': 'b' }
-
+ => { '1': 'c', '2': 'b' }
 Implement this function and then create Jasmine tests to verify its 
 correctness. Think of all the input that could come in and decide
 what to do with them via tests.
 */
 
-function invert() {}
+function invert(obj) { 
+    let inverted = {};
+    
+    // Swapping key with value
+    for (let [key, value] of Object.entries(obj)) {
+        inverted[value] = key;
+    }
+
+    return inverted;
+}
+
+module.exports = invert;
