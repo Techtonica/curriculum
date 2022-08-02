c86235af34ce592159f1b726e8e41aca52103ead
Priyaraj7 <suppichala@gmail.com>
2022-08-02 16:44:21 -0700

    move code inside the function

--- pair-programming/week-2/newspaper-list/script.js
+++ pair-programming/week-2/newspaper-list/script.js
@@ -4,15 +4,24 @@ const newspapers = [
   "http://www.economist.com"
 ];
 
-let ul = document.createElement("ul");
+function listNewsPaper(arr) {
+  let divContent = document.getElementById("content");
+  let ulEle = document.createElement("ul");
+  // loop through the arr of newspaper
+  for (i = 0; i < arr.length; i++) {
+    let liEle = document.createElement("li");
+    let aTag = document.createElement("a");
+    // appending ul to div
+    divContent.appendChild(ulEle);
+    //  appending li to ul
+    ulEle.appendChild(liEle);
+    //  appending anchor tag to li
+    liEle.appendChild(aTag);
 
-document.getElementById("content").appendChild(ul);
+    aTag.href = arr[i];
 
-newspapers.forEach(function (item) {
-  let li = document.createElement("li");
-  ul.appendChild(li);
-  let a = document.createElement("a");
-  li.appendChild(a);
-  a.href = item;
-  a.innerText = item;
-});
+    aTag.innerText = arr[i];
+  }
+}
+// calling function
+listNewsPaper(newspapers);
