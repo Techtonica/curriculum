fb1d7d7bda1a9da6dcb711c1675dcdf3bd027982
Daaimah Tibrey <41805952+daaimah123@users.noreply.github.com>
2025-08-07 10:17:08 -0700

    Create test/database.test.js

---
+++ testing-and-tdd/todo-api-integration-testing-project/test/database.test.js
@@ -0,0 +1,35 @@
+// TODO: Step 3 - Database interaction tests
+const database = require('../database');
+
+describe('Database Tests', function() {
+  
+  // TODO: Add setup and teardown for test database
+  before(function() {
+    // HINT: Set up test database connection
+    // Consider using a separate test database
+  });
+  
+  after(function() {
+    // HINT: Clean up test database
+  });
+  
+  describe('getTodos', function() {
+    it('should retrieve all todos from database', function(done) {
+      // TODO: Implement database test
+      // HINT: This tests the actual database interaction
+      
+      // YOUR CODE HERE
+      done(); // Remove this when you implement the test
+    });
+  });
+  
+  describe('createTodo', function() {
+    it('should insert new todo into database', function(done) {
+      // TODO: Implement database creation test
+      // HINT: Create a todo and verify it was inserted
+      
+      // YOUR CODE HERE
+      done(); // Remove this when you implement the test
+    });
+  });
+});
