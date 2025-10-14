3f9ffe0606dbf20a9331383fd6e580d821abb08d
Daaimah Tibrey <41805952+daaimah123@users.noreply.github.com>
2025-10-14 10:22:05 -0700

    Update index.js

---
+++ pair-programming/week-1/Modeling-A-Bank-Account/index.js
@@ -0,0 +1,13 @@
+// Write a program that creates an `account` object with the following characteristics:
+    // * A `name` property.
+    // * A `balance` property set to 0 
+  // * A `deposit` method adding the (positive or negative) value passed as an argument to the account balance.
+  // * A `describe` method that prints the account description in the console.
+  // * A `transfer` method with two parameters: the name of the account that will receive the transfer, and the amount of money to transfer. 
+
+  
+// ## Test
+// - Create an account for Billy, Rosie, Jack and Jill
+// - Give each of the accounts a deposit on money
+// - Print a string describing the current amount of money on each account
+// - Transfer positive values between Billy and Jack and negative values between Rosie and Jack
