bad360b8499761ccf759fb9988a09a1d01304584
Daaimah Tibrey <41805952+daaimah123@users.noreply.github.com>
2025-11-18 10:59:02 -0800

    Update Weather.jsx

--- pair-programming/week-5/Weather_Forecast_App/src/Weather.jsx
+++ pair-programming/week-5/Weather_Forecast_App/src/Weather.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const Weather = () => {
+  // what is useState used for
+  // if there is time explore what useEffect is and how it's different from useState, you will need it in the future
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
 
@@ -13,18 +15,14 @@ const Weather = () => {
   const handleSubmit = (event) => {};
 
   return (
-    // Add a form input that fetches weather data based on the city name entered by the user
-    // Display the city's weather details on your page (description, wind speed, humidity, temperature, etc)
-    // [Bonus] handle errors gracefully and consider edge cases, such as invalid input or issues with the API request
-    <div>
-      {weatherData ? (
-        <>
-          <h2>{weatherData.name}</h2>
-        </>
-      ) : (
-        <p>Loading weather data...</p>
-      )}
-    </div>
+    {/* 
+    
+    Add a form input that fetches weather data based on the city name entered by the user
+      for example: if data passed through show name otherwise load data
+    Display the city's weather details on your page (description, wind speed, humidity, temperature, etc)
+    [Bonus] handle errors gracefully and consider edge cases, such as invalid input or issues with the API request
+    
+    */}
   );
 };
 
