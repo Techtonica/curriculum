6919492de1fad992a7768f23eaf079498c8ba7f3
Olexandr88 <radole1203@gmail.com>
2026-01-29 12:20:42 +0200

    eslint: ignore nested dist directories

--- pair-programming/week-7/react-forms-continued/eslint.config.js
+++ pair-programming/week-7/react-forms-continued/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["**/dist/**"]),
   {
     files: ["**/*.{js,jsx}"],
     extends: [
